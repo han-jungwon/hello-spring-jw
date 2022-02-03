4648d0f7be1ada6b3d04ec45fa99e0d0a1a23acc
han-jungwon <rhehf1024@naver.com>
2022-02-04 01:13:17 +0900

    빌드하고 실행하기까지

---
+++ src/main/java/com/hello/hellospringjw/HelloSpringJwApplication.java
@@ -0,0 +1,13 @@
+package com.hello.hellospringjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringJwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringJwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello/hellospringjw/HelloSpringJwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.hellospringjw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringJwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
