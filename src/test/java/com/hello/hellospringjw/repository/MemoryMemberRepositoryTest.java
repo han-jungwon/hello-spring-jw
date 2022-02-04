1a1a58b50503e6a9adfe6866b4de86ca0dc5570d
han-jungwon <rhehf1024@naver.com>
2022-02-04 17:49:40 +0900

    회원 도메인과 리포지토리 만들기까지

---
+++ src/test/java/com/hello/hellospringjw/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.hello.hellospringjw.repository;
+
+import com.hello.hellospringjw.domain.Member;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        System.out.println("result = " + (result == member));
+
+    }
+}
